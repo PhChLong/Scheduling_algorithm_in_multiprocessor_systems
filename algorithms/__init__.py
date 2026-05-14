d5e9414aad1d0af9a8589778adc006c84c881df9
trantatdat123 <trantatdatttd@gmail.com>
2026-05-14 23:47:55 +0700

    chinh packages lai de import cho de

---
+++ algorithms/__init__.py
@@ -0,0 +1,3 @@
+from .GLB_RR import GLB_RR
+from .schedule import Schedule
+from .schedule_step import ScheduleStep
--- algorithms/schedule_step.py
+++ algorithms/schedule_step.py
@@ -5,6 +5,7 @@ class ScheduleStep:
     process_id: int
     begin_time: int
     end_time:   int
+    cpu_id: int
 
     def __repr__(self):
-        return f"ScheduleStep(pid={self.process_id}, begin={self.begin_time}, end={self.end_time})"
+        return f"ScheduleStep(pid={self.process_id}, begin={self.begin_time}, end={self.end_time}, cpu={self.cpu_id})"
